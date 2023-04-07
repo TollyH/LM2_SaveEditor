7050f033526254e64daf6548191dc0cf55a2959d
TollyH <ptolemyhill@sky.com>
2023-04-07 14:47:46 +0100

    Create signature for checksum method

---
+++ SaveTools/CRC.cs
@@ -0,0 +1,10 @@
+﻿namespace LM2.SaveTools
+{
+    public static class CRC
+    {
+        public static uint CalculateChecksum(uint initial, byte[] source, int sourceOffset, int bytesToProcess)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
