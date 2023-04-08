2f838e5a7487f6fe0cd3d8e28d2e3a5eed698323
TollyH <ptolemyhill@sky.com>
2023-04-08 14:51:40 +0100

    Implement read and write methods

---
+++ SaveTools/FileOperations.cs
@@ -0,0 +1,15 @@
+﻿namespace LM2.SaveTools
+{
+    public static class FileOperations
+    {
+        public static SaveData ReadSaveData(string filepath, bool ignoreCRC = false)
+        {
+            return new SaveData(File.ReadAllBytes(filepath), ignoreCRC);
+        }
+
+        public static void WriteSaveData(string filepath, SaveData saveData)
+        {
+            File.WriteAllBytes(filepath, saveData.GetBytes());
+        }
+    }
+}
