1b767ba94251c5208b7bafb2e082716ca813b24e
TollyH <ptolemyhill@sky.com>
2023-04-10 19:49:29 +0100

    Add file operations menu

--- SaveEditor/MainWindow.xaml.cs
+++ SaveEditor/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using LM2.SaveTools;
+using Microsoft.Win32;
+using System.Windows;
 
 namespace LM2.SaveEditor
 {
@@ -7,9 +9,66 @@ namespace LM2.SaveEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SaveData? loadedSave = null;
+        private string loadedSavePath = "";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openDialog = new()
+            {
+                Filter = "Save File|*.sav",
+                CheckPathExists = true,
+                CheckFileExists = true
+            };
+            if (!openDialog.ShowDialog() ?? true)
+            {
+                return;
+            }
+            loadedSave = FileOperations.ReadSaveData(openDialog.FileName);
+            loadedSavePath = openDialog.FileName;
+            mainTabControl.Visibility = Visibility.Visible;
+        }
+
+        private void SaveItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (loadedSave is null)
+            {
+                return;
+            }
+            FileOperations.WriteSaveData(loadedSavePath, loadedSave);
+        }
+
+        private void SaveAsItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (loadedSave is null)
+            {
+                return;
+            }
+            SaveFileDialog saveDialog = new()
+            {
+                AddExtension = true,
+                DefaultExt = ".sav",
+                Filter = "Save File|*.sav",
+                CheckPathExists = true,
+                FileName = loadedSavePath
+            };
+            if (!saveDialog.ShowDialog() ?? true)
+            {
+                return;
+            }
+            FileOperations.WriteSaveData(saveDialog.FileName, loadedSave);
+        }
+
+        private void CloseItem_Click(object sender, RoutedEventArgs e)
+        {
+            loadedSave = null;
+            loadedSavePath = "";
+            mainTabControl.Visibility = Visibility.Collapsed;
         }
     }
 }
