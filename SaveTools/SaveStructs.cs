a59cbbfc8255140ddfb690f4778188742b413c03
TollyH <ptolemyhill@sky.com>
2023-04-10 19:04:02 +0100

    Implement save struct methods

--- SaveTools/SaveStructs.cs
+++ SaveTools/SaveStructs.cs
@@ -12,14 +12,14 @@
         public Grade Grade { get; set; }
     }
 
-    public struct BasicGhost
+    public struct BasicGhostInfo
     {
         public byte NumCollected { get; set; }
         public ushort MaxWeight { get; set; }
         public bool IsNew { get; set; }
     }
 
-    public struct Ghost
+    public struct GhostInfo
     {
         public bool Collected { get; set; }
         public byte NumCollected { get; set; }
@@ -27,15 +27,139 @@
         public bool IsNew { get; set; }
     }
 
-    public struct Gem
+    public struct GemInfo
     {
         public bool Collected { get; set; }
         public bool IsNew { get; set; }
     }
 
-    public struct EndlessMode
+    public struct EndlessModeInfo
     {
         public bool Unlocked { get; set; }
         public byte HighestFloor { get; set; }
+    }
+
+    public partial class SaveData
+    {
+        public partial class GameData
+        {
+            public MissionInfo[] GetMissionInfo()
+            {
+                List<MissionInfo> list = new();
+                for (int i = 0; i < MissionCompletion.Length; i++)
+                {
+                    list.Add(new MissionInfo()
+                    {
+                        Completed = MissionCompletion[i],
+                        Locked = MissionLocked[i],
+                        BooCaptured = MissionBooCaptured[i],
+                        FastestTime = MissionClearTime[i],
+                        GhostsCaptured = MissionGhostsCaptured[i],
+                        DamageTaken = MissionDamageTaken[i],
+                        TreasureCollected = MissionTreasureCollected[i],
+                        Grade = MissionGrade[i]
+                    });
+                }
+                return list.ToArray();
+            }
+
+            public BasicGhostInfo[] GetBasicGhostInfo()
+            {
+                List<BasicGhostInfo> list = new();
+                for (int i = 0; i < NumBasicGhostCollected.Length; i++)
+                {
+                    list.Add(new BasicGhostInfo()
+                    {
+                        NumCollected = NumBasicGhostCollected[i],
+                        MaxWeight = MaxBasicGhostWeight[i],
+                        IsNew = BasicGhostNotifyState[i] == 2
+                    });
+                }
+                return list.ToArray();
+            }
+
+            public GhostInfo[] GetGhostInfo()
+            {
+                List<GhostInfo> list = new();
+                for (int i = 0; i < NumGhostCollected.Length; i++)
+                {
+                    list.Add(new GhostInfo()
+                    {
+                        Collected = GhostCollectableState[i] == 2,
+                        NumCollected = NumGhostCollected[i],
+                        MaxWeight = MaxGhostWeight[i],
+                        IsNew = GhostNotifyState[i] == 2
+                    });
+                }
+                return list.ToArray();
+            }
+
+            public GemInfo[] GetGemInfo()
+            {
+                List<GemInfo> list = new();
+                for (int i = 0; i < GemCollected.Length; i++)
+                {
+                    list.Add(new GemInfo()
+                    {
+                        Collected = GemCollected[i],
+                        IsNew = GemNotifyState[i] == 2
+                    });
+                }
+                return list.ToArray();
+            }
+
+            public EndlessModeInfo[] GetEndlessModeInfo()
+            {
+                List<EndlessModeInfo> list = new();
+                for (int i = 0; i < EndlessFloorsUnlocked.Length; i++)
+                {
+                    list.Add(new EndlessModeInfo()
+                    {
+                        Unlocked = EndlessFloorsUnlocked[i],
+                        HighestFloor = EndlessModeHighestFloorReached[i]
+                    });
+                }
+                return list.ToArray();
+            }
+
+            public void UpdateFromMissionInfo(int index, MissionInfo missionInfo)
+            {
+                MissionCompletion[index] = missionInfo.Completed;
+                MissionLocked[index] = missionInfo.Locked;
+                MissionBooCaptured[index] = missionInfo.BooCaptured;
+                MissionClearTime[index] = missionInfo.FastestTime;
+                MissionGhostsCaptured[index] = missionInfo.GhostsCaptured;
+                MissionDamageTaken[index] = missionInfo.DamageTaken;
+                MissionTreasureCollected[index] = missionInfo.TreasureCollected;
+                MissionGrade[index] = missionInfo.Grade;
+            }
+
+            public void UpdateFromBasicGhostInfo(int index, BasicGhostInfo basicGhostInfo)
+            {
+                NumBasicGhostCollected[index] = basicGhostInfo.NumCollected;
+                MaxBasicGhostWeight[index] = basicGhostInfo.MaxWeight;
+                BasicGhostNotifyState[index] = (byte)(basicGhostInfo.IsNew ? 2 : 0);
+            }
+
+            public void UpdateFromGhostInfo(int index, GhostInfo ghostInfo)
+            {
+                GhostCollectableState[index] = (byte)(ghostInfo.Collected ? 2 : 0);
+                NumGhostCollected[index] = ghostInfo.NumCollected;
+                MaxGhostWeight[index] = ghostInfo.MaxWeight;
+                GhostNotifyState[index] = (byte)(ghostInfo.IsNew ? 2 : 0);
+            }
+
+            public void UpdateFromGemInfo(int index, GemInfo gemInfo)
+            {
+                GemCollected[index] = gemInfo.Collected;
+                GemNotifyState[index] = (byte)(gemInfo.IsNew ? 2 : 0);
+            }
+
+            public void UpdateFromEndlessModeInfo(int index, EndlessModeInfo endlessModeInfo)
+            {
+                EndlessFloorsUnlocked[index] = endlessModeInfo.Unlocked;
+                EndlessModeHighestFloorReached[index] = endlessModeInfo.HighestFloor;
+            }
+        }
     }
 }
